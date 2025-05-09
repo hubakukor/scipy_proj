06ef313730f9ebac4345affdde3d366ffc02a2f6
hubakukor <huba.kukor@gmail.com>
2025-05-09 08:54:24 +0200

    updating docstrings

--- eeg_toolbox/visual.py
+++ eeg_toolbox/visual.py
@@ -3,6 +3,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def plot_eeg_overview(data, ch_names, sfreq, n_channels=5, offset=100):
+    """
+    Plots the first n EEG channels with an offset on the time axis.
+
+    Args:
+        data: The EEG data in a numpy array format.
+        ch_names: List containing the names of the EEG channels.
+        sfreq: Sampling frequency of the EEG data.
+        n_channels: The first n channels of the EEG data that appears on the plots. (Default: 5)
+        offset: Customize the offset of the data on the plot. (Default: 100)
+
+    Returns:
+
+    """
     times = np.arange(data.shape[1]) / sfreq
     data_scaled = data * 1e6
     plt.figure(figsize=(14, 6))
@@ -16,6 +29,18 @@ def plot_eeg_overview(data, ch_names, sfreq, n_channels=5, offset=100):
     plt.show()
 
 def plot_fft_spectrum(mean_fft, sfreq, n_samples):
+    """
+    Displays the average frequency spectrum of the EEG data (Fourier transform).
+    Requires the returned data from data_fourier() function.
+
+    Args:
+        mean_fft: The fourier transformed EEG data (returned from data_fourier() function).
+        sfreq: Sampling frequency of the EEG data.
+        n_samples: The number of data points in the EEG data.
+
+    Returns:
+
+    """
     freqs = np.fft.fftfreq(n_samples, 1 / sfreq)
     plt.figure(figsize=(10, 4))
     plt.plot(freqs[:len(freqs)//2], mean_fft[:len(freqs)//2])
@@ -28,6 +53,20 @@ def plot_fft_spectrum(mean_fft, sfreq, n_samples):
     plt.show()
 
 def plot_graph_segments(data, sfreq, events, ch_names, window_sec=1.5, n_show=5):
+    """
+    Plots the EEG bands detected in the data with a custom size window around the detected events. Requires the returned data from detect_graphoelements() function.
+
+    Args:
+        data:
+        sfreq: Sampling frequency of the EEG data.
+        events: The detected EEG bands from detect_graphoelements().
+        ch_names: A list containing the names of the EEG channels. Returned from load_edf_data().
+        window_sec: The plot visualizes a +- window_sec second window around the detected events. Default: 1.5
+        n_show:
+
+    Returns:
+
+    """
     window_samples = int(window_sec * sfreq)
     plt.figure(figsize=(14, n_show * 3))
     for i, event in enumerate(events[:n_show]):
